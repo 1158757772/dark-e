12f1dc5c0939cecac26ed17cbea3b17480bf75bd
$zh <1158757772@qq.com>
2020-10-19 13:26:18 +0800

    项目开始

---
+++ public/js/city.js
@@ -0,0 +1,34 @@
+$ul=$('#city-list');
+$city=$('#city');
+$op=$('#op');
+$bg=$('#bg');
+$op.fadeToggle(1000);
+$city.on('click',()=>{
+    $ul.slideToggle(500);
+})
+$ul.on('click','li',function(){
+    
+    turn($(this)).then(()=>{
+            return turn_2()
+    }).then(()=>{
+        location.href="./index.html"
+    })
+});
+function turn(_this){
+    return new Promise((resolve,reject)=>{
+        $city.children().eq(0).html(_this.html());
+        $ul.slideToggle(500);
+        
+        $op.fadeToggle(1000);
+        setTimeout(resolve,1500);
+    })
+  }
+  function turn_2(){
+    return new Promise((resolve,reject)=>{
+        $bg.css({
+            background: `url("http://www.blackswancake.com/statics/images/loging.gif") no-repeat center `
+        });
+        setTimeout(resolve,1200);
+    })
+    
+  }
