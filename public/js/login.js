438950d6b449c2562044b3ecf388347ea63a1b0c
xiaoming <916103747@qq.com>
2020-10-20 13:34:16 +0800

    添加api文档

--- public/js/login.js
+++ public/js/login.js
@@ -5,19 +5,17 @@ $('input').on( 'click' , function (){
 } )
 
 $('.login').on( 'click' , function (){
-    
-
     let url = 'http://localhost:3000/user/login';
     let user = $('.user').val();
     let pass = $('.pass').val();
-
+    
     $.post( url , { user , pass } , function ( data ){
         if( data.err !== 2 ){
             alert( data.msg );
         }
         else{
             alert( data.msg );
-
+            
             location.href = '../html/city.html'
         }
     } )
